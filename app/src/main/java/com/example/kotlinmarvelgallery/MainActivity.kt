6983f90362bb9c3fcb49d1e7c7cd727134b8e554
webprog26 <webprog26@gmail.com>
2019-04-14 16:11:23 +0300

    Binding variable to the View

--- app/src/main/java/com/example/kotlinmarvelgallery/MainActivity.kt
+++ app/src/main/java/com/example/kotlinmarvelgallery/MainActivity.kt
@@ -5,12 +5,15 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
 import com.example.kotlinmarvelgallery.data.Doctor
+import com.example.kotlinmarvelgallery.utils.bindToText
 import com.example.kotlinmarvelgallery.utils.bindView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), MainView {
 
-    val tvText: TextView by bindView(R.id.tv_text)
-    val btnClick: Button by bindView(R.id.btn_click)
+    override var name: String by bindToText(R.id.tv_text)
+
+    private val tvText: TextView by bindView(R.id.tv_text)
+    private val btnClick: Button by bindView(R.id.btn_click)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
--- app/src/main/java/com/example/kotlinmarvelgallery/utils/Utils.kt
+++ app/src/main/java/com/example/kotlinmarvelgallery/utils/Utils.kt
@@ -3,8 +3,12 @@ package com.example.kotlinmarvelgallery.utils
 import android.app.Activity
 import android.content.Context
 import android.os.Parcelable
+import android.support.annotation.IdRes
 import android.view.View
+import android.widget.TextView
 import android.widget.Toast
+import kotlin.properties.ReadWriteProperty
+import kotlin.reflect.KProperty
 
 fun <T : View> Activity.bindView(viewId: Int) = lazy { findViewById(viewId) as T }
 
@@ -14,4 +18,18 @@ fun Activity.extraString(key: String) = lazy { intent.extras.getString(key) }
 
 fun Context.toast(message: String, length: Int) {
     Toast.makeText(this, message, length).show()
+}
+
+fun Activity.bindToText(
+    @IdRes viewId: Int) = object :
+    ReadWriteProperty<Any?, String> {
+    val textView by lazy { findViewById<TextView>(viewId) }
+
+    override fun getValue(thisRef: Any?, property: KProperty<*>): String {
+        return textView.text.toString()
+    }
+
+    override fun setValue(thisRef: Any?, property: KProperty<*>, value: String) {
+        textView.text = value
+    }
 }
