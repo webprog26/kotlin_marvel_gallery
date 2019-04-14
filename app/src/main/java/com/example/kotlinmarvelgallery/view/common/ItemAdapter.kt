2c66cdee44582696406f5e5ffcf2cd27d368d305
webprog26 <webprog26@gmail.com>
2019-04-14 19:28:01 +0300

    List of items implemented

---
+++ app/src/main/java/com/example/kotlinmarvelgallery/view/common/ItemAdapter.kt
@@ -0,0 +1,18 @@
+package com.example.kotlinmarvelgallery.view.common
+
+import android.support.annotation.LayoutRes
+import android.support.v7.widget.RecyclerView
+import android.view.View
+
+abstract class ItemAdapter<T: RecyclerView.ViewHolder>
+    (@LayoutRes open val layoutId: Int) {
+
+    abstract fun onCreateViewHolder(itemView: View) : T
+
+    @Suppress("UNCHECKED_CAST")
+    fun bindViewHolder(holder: RecyclerView.ViewHolder) {
+        (holder as T).onBindViewHolder()
+    }
+
+    abstract fun T.onBindViewHolder()
+}
